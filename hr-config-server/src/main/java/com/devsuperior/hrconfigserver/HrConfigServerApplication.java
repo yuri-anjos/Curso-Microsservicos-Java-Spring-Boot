a1cb20a90aeb4716555fdc1f24a96c850c3ea12c
yuri-anjos <yuri.dos.4njos@gmail.com>
2023-06-03 17:04:54 -0300

    feat: hr-api-config created

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hrworker;
+package com.devsuperior.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
