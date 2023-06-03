860bc5507366752791d003aa24ea05d84ef89301
yuri-anjos <yuri.dos.4njos@gmail.com>
2023-06-03 13:42:55 -0300

    feat: configure eureka clients

--- hr-worker/src/main/java/com/devsuperior/hreurekaserver/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hreurekaserver/HrWorkerApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrWorkerApplication {
 
     public static void main(String[] args) {
