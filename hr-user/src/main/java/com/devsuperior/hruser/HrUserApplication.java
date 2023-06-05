3c91848b2187f2a7411ffda114a10632f394690e
yuri-anjos <yuri.dos.4njos@gmail.com>
2023-06-05 10:43:01 -0300

    feat: user, role and db seeding

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrUserApplication {
 
     public static void main(String[] args) {
