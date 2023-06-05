527e397ce77583aa2cf3f512419b45dddf56663b
yuri-anjos <yuri.dos.4njos@gmail.com>
2023-06-05 11:19:58 -0300

    feat: hr-oauth creation

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/HrOAuthApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HrOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOAuthApplication.class, args);
+    }
+
+}
