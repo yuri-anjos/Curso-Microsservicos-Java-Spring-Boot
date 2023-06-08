b8a96d1d9b7350fca34a030379bcead5b1459146
yuri-anjos <yuri.dos.4njos@gmail.com>
2023-06-08 13:38:53 -0300

    feat: remove test.config

--- hr-worker/src/main/java/com/devsuperior/hreurekaserver/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hreurekaserver/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.devsuperior.hreurekaserver.services.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,19 +21,11 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfigs;
-
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerService workerService;
-
-    @GetMapping(value = "/configs")
-    public String getConfig() {
-        return "Config: " + testConfigs;
-    }
 
     @GetMapping
     public List<Worker> findAll() {
